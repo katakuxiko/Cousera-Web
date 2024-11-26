73c197eea04dad0df2db2f23be0beefb8e4f2017
aleksei vdovenko <avxmvd@gmail.com>
2024-11-26 11:08:25 +0500

    feat: lab 4

--- index.js
+++ index.js
@@ -12,3 +12,44 @@ menu.addEventListener("click", (e) => {
 	}
 	console.log(menu.className);
 });
+
+// STEP 1: Объявляем массив имён
+const names = [
+	"Yaakov",
+	"John",
+	"Jen",
+	"Jason",
+	"Paul",
+	"Frank",
+	"Larry",
+	"Paula",
+	"Laura",
+	"Jim",
+];
+
+// STEP 2: Создаём объект 'helloSpeaker'
+const helloSpeaker = {
+	speak: function (name) {
+		console.log("Hello " + name);
+	},
+};
+
+// STEP 3: Создаём объект 'byeSpeaker'
+const byeSpeaker = {
+	speak: function (name) {
+		console.log("Goodbye " + name);
+	},
+};
+
+// STEP 4: Перебираем массив имён и выводим сообщения
+for (let i = 0; i < names.length; i++) {
+	// Берём первую букву имени
+	const firstLetter = names[i][0].toLowerCase();
+
+	// Проверяем, начинается ли имя с 'j'
+	if (firstLetter === "j") {
+		byeSpeaker.speak(names[i]);
+	} else {
+		helloSpeaker.speak(names[i]);
+	}
+}
